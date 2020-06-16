2991b4ae3ede8c33a3fd2bd7a91b836267f8ccdd
zoranildosantos <zoranildo22@gmail.com>
2020-06-15 21:51:56 -0300

    Conclusão

--- src/services/user.js
+++ src/services/user.js
@@ -7,11 +7,11 @@ module.exports = app => {
       if(!user.name) return { error: 'Nome é um atributo obrigatório.' }
       if(!user.email) return { error: 'Email é um atributo obrigatório.' }
       if(!user.password) return { error: 'Senha é um atributo obrigatório.' }
-      
+
       const userDb = await findAll({email: user.email})
       if(userDb && userDb.length > 0) return {error: 'Já existe um usuário com esse email!'}
 
-        return app.db('users').insert(user, '*')
+      return app.db('users').insert(user, '*')
     }
 
     return { findAll, save }
